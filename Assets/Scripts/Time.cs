477a714acbe97ada277e7faf7f85861d21cb3959
RenciSupe <123269716+RenciSupe@users.noreply.github.com>
2023-04-22 16:50:50 -0600

    Create Time.cs

---
+++ Assets/Scripts/Time.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class TimeTracker : MonoBehaviour {
+ 
+    public static List <long> CheckpointTimes = new List <long>;
+    public static long TimeCnt = 0;
+    public bool Checkpoint = false;
+    private int i = 0;
+ 
+ 
+    void Update () {
+        TimeCnt += Time.deltaTime;
+        if(Checkpoint == true){
+          CheckpointTimes[i++] = TimeCnt;
+          Checkpoint = false;
+        }
+ 
+ 
+    }
+}
