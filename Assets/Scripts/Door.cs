a46c900f0efbd16c3f433d4817ff7283ce67da1b
tymber02 <89433391+tymber02@users.noreply.github.com>
2023-04-23 19:41:51 -0600

    Upload Additional Files

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,31 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+
 
 public class Door : MonoBehaviour
 {
-    public GameObject cauldron;
-    public bool isUnlocked = false;
-    public bool isOpen = false;
+    public Cauldron cauldron;
+    public CharacterController playerController;
+    private bool isUnlocked = false;
+    private bool isOpen = false;
     public Animator anim;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static float minDistance = 5.5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("isUnlocked = " + isUnlocked);
+        print("Player Distance: " + getPlayerDistance());
+        print("Within proximity: " + (getPlayerDistance() <= minDistance));
+        if(isUnlocked && getPlayerDistance() <= minDistance) {
+            isOpen = true;
+            anim.SetBool("isOpened", true);
+        }
+        if(isOpen) {
+            anim.SetBool("isOpened", true);
+        }
+        // else do nothing
     }
 
-    void onClick() {
+    /*void onClick() {
         if(isUnlocked) {
             isOpen = true;
             anim.SetBool("isOpened", true);
         }
         // else do nothing
+    }*/
+
+    private float getPlayerDistance() {
+        Vector3 offset = transform.position - playerController.transform.position;
+        return (float)(Math.Sqrt(Math.Pow(offset.x, 2) + Math.Pow(offset.y, 2) + Math.Pow(offset.z, 2)));
+    }
+    
+    public void unlock() {
+        isUnlocked = true;
+    }
+
+    public bool open() {
+        return isOpen;
     }
 }
