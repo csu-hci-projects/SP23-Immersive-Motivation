8bd417e28fbdffa88e6c17f8c557a7bf502e8289
RenciSupe <123269716+RenciSupe@users.noreply.github.com>
2023-04-25 20:58:14 -0600

    Update Cauldron.cs
    Added else ifs so that only one of the conditions will drive the color of the cauldron and TimeTracker function calls.

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -25,36 +25,45 @@ public class Cauldron : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(door1.open()) {
-            cauldron.GetComponent<Renderer>().material.color = new Color(218, 159, 55, 255);
-            door2.unlock();
-        }
-        if(door2.open()) {
-            cauldron.GetComponent<Renderer>().material.color = new Color(92, 151, 193, 255);
-            door3.unlock();
-        }
-        if(door3.open()) {
-            cauldron.GetComponent<Renderer>().material.color = new Color(69, 155, 118, 255);
-            door4.unlock();
-        }
-        if(door4.open()) {
-            cauldron.GetComponent<Renderer>().material.color = new Color(238, 227, 97, 255);
-            door5.unlock();
-        }
-        if(door5.open()) {
-            cauldron.GetComponent<Renderer>().material.color = new Color(46, 114, 173, 255);
-            door6.unlock();
-        }
-        if(door6.open()) {
-            cauldron.GetComponent<Renderer>().material.color = new Color(199, 100, 38, 255);
-            door7.unlock();
-        }
-        if(door7.open()) {
-            cauldron.GetComponent<Renderer>().material.color = new Color(178, 114, 152, 255);
-            door8.unlock();
-        }
         if(door8.open()) {
             // Open Completion UI
+            ExportTimeData();
+        }
+        else if(door7.open()) {
+            cauldron.GetComponent<Renderer>().material.color = new Color(178, 114, 152, 255);
+            door8.unlock();
+            CheckpointReached();
+        }
+        else if(door6.open()) {
+            cauldron.GetComponent<Renderer>().material.color = new Color(199, 100, 38, 255);
+            door7.unlock();
+            CheckpointReached();
+        }
+        else if(door5.open()) {
+            cauldron.GetComponent<Renderer>().material.color = new Color(46, 114, 173, 255);
+            door6.unlock();
+            CheckpointReached();
+        }
+        else if(door4.open()) {
+            cauldron.GetComponent<Renderer>().material.color = new Color(238, 227, 97, 255);
+            door5.unlock();
+            CheckpointReached();
+        }
+        
+        else if(door3.open()) {
+            cauldron.GetComponent<Renderer>().material.color = new Color(69, 155, 118, 255);
+            door4.unlock();
+            CheckpointReached();
+        }
+        else if(door2.open()) {
+            cauldron.GetComponent<Renderer>().material.color = new Color(92, 151, 193, 255);
+            door3.unlock();
+            CheckpointReached();
+        }
+        else if(door1.open()) {
+            cauldron.GetComponent<Renderer>().material.color = new Color(218, 159, 55, 255);
+            door2.unlock();
+            CheckpointReached();
         }
     }
 }
