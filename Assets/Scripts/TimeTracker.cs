c834b6cdc0e84241f5ffb0836f66cb2722d76aea
RenciSupe <123269716+RenciSupe@users.noreply.github.com>
2023-04-25 19:46:34 -0600

    Update TimeTracker.cs
    Fixed my silly errors

--- Assets/Scripts/TimeTracker.cs
+++ Assets/Scripts/TimeTracker.cs
@@ -4,12 +4,18 @@ using System.IO;
  
 public class TimeTracker : MonoBehaviour {
  
-    public static List <long> CheckpointTimes = new List <long>;
-    public static long TimeCnt = 0;
-    private int i = 0;
+    public static List <float> CheckpointTimes;
+    public static float TimeCnt;
+    private int i;
+
+    void Start(){
+        CheckpointTimes = new List <float>();
+        TimeCnt = 0;
+        i = 0;
+    }
  
  
-    void Update () {
+    void Update() {
         TimeCnt += Time.deltaTime;
     }
     public void ReachedCheckpoint(){
@@ -22,9 +28,10 @@ public class TimeTracker : MonoBehaviour {
     }
  //referenced https://support.unity.com/hc/en-us/articles/115000341143-How-do-I-read-and-write-data-from-a-text-file-
     public void ExportTimeData(){
+      string path = "Assets/CheckpointTimes.txt";
+
       StreamWriter writer = new StreamWriter(path, true);
       foreach (long t in CheckpointTimes){
-        string path = "Assets/CheckpointTimes.txt";
         //Write some text to the test.txt file
         
         writer.WriteLine("\nTime: ",t);
@@ -32,8 +39,8 @@ public class TimeTracker : MonoBehaviour {
       }
      writer.Close();
         //Re-import the file to update the reference in the editor
-     AssetDatabase.ImportAsset(path); 
-     TextAsset asset = Resources.Load("CheckpointTimes");
+     //AssetDatabase.ImportAsset(path); 
+     TextAsset asset = (TextAsset)Resources.Load("CheckpointTimes");
         //Print the text from the file
      Debug.Log(asset.text);
     }
